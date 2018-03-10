25ee96ed285e15788f4b3ce4bc4ddb150117424f
Trust Mubaiwa <trust.mubaiwa@momentum.co.za>
2018-03-10 11:28:37 +0200

    prepare for recycler vire adapter

--- app/src/main/java/com/example/trmubaiwa/appone/Activities/ShopActivity.kt
+++ app/src/main/java/com/example/trmubaiwa/appone/Activities/ShopActivity.kt
@@ -2,14 +2,13 @@ package com.example.trmubaiwa.appone.Activities
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.support.v7.app.AppCompatActivity
 import com.example.trmubaiwa.appone.Adapters.CategoryAdapter
-import com.example.trmubaiwa.appone.Models.Category
 import com.example.trmubaiwa.appone.R
 import com.example.trmubaiwa.appone.Services.DataService
 import kotlinx.android.synthetic.main.activity_shop.*
+import org.jetbrains.anko.longToast
 
 class ShopActivity : AppCompatActivity() {
 
@@ -22,6 +21,12 @@ class ShopActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this,DataService.categories)
 
         categoryListView.adapter = adapter
+
+
+        categoryListView.setOnItemClickListener { adapterView, view, position, id ->
+            val category = DataService.categories[position]
+            longToast("Yo cliecked on  the ${category.title} item") // anko library
+        }
 
 
     }
