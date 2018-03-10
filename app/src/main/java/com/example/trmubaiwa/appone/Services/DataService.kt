1cf9ce61c77522524ee6aee2c58c5dc80b0ba02d
Trust Mubaiwa <trust.mubaiwa@momentum.co.za>
2018-03-10 15:52:29 +0200

    changed DataSer

--- app/src/main/java/com/example/trmubaiwa/appone/Services/DataService.kt
+++ app/src/main/java/com/example/trmubaiwa/appone/Services/DataService.kt
@@ -10,41 +10,52 @@ object DataService {
 
     val categories = listOf(
             Category("Shirts", "shirstimage"),
-            Category("Hoodies","hoodiesimage"),
-            Category("hats","hatimage"),
+            Category("Hoodies", "hoodiesimage"),
+            Category("hats", "hatimage"),
             Category("Shirts", "shirstimage"),
-            Category("Hoodies","hoodiesimage"),
-            Category("hats","hatimage"),
+            Category("Hoodies", "hoodiesimage"),
+            Category("hats", "hatimage"),
             Category("Shirts", "shirstimage"),
-            Category("Hoodies","hoodiesimage"),
-            Category("hats","hatimage"),
+            Category("Hoodies", "hoodiesimage"),
+            Category("hats", "hatimage"),
             Category("Shirts", "shirstimage"),
-            Category("Hoodies","hoodiesimage"),
-            Category("hats","hatimage"),
+            Category("Hoodies", "hoodiesimage"),
+            Category("hats", "hatimage"),
             Category("Shirts", "shirstimage"),
-            Category("Hoodies","hoodiesimage"),
-            Category("hats","hatimage"),
+            Category("Hoodies", "hoodiesimage"),
+            Category("hats", "hatimage"),
             Category("Digitl", "digitlagoodsmage")
     )
 
     val hats = listOf(
-            Product("Fallen Black hat","$23","blackhat"),
-            Product("Fallen Red hat","$22","redhat"),
-            Product("Fallen Grey hat","$21","greyhat")
+            Product("Fallen Black hat", "$23", "blackhat"),
+            Product("Fallen Red hat", "$22", "redhat"),
+            Product("Fallen Grey hat", "$21", "greyhat")
     )
 
     val hoodies = listOf(
-            Product("Fallen Hoodie Grey","$20","greyhoodie"),
-            Product("Fallen Hoodie Red","$18","redhoodie"),
-            Product("Fallen Hoodie Yellow","$21","yellowhoodie"),
-            Product("Fallen Hoodie Black","$15","blackhoodie")
+            Product("Fallen Hoodie Grey", "$20", "greyhoodie"),
+            Product("Fallen Hoodie Red", "$18", "redhoodie"),
+            Product("Fallen Hoodie Yellow", "$21", "yellowhoodie"),
+            Product("Fallen Hoodie Black", "$15", "blackhoodie")
     )
 
 
-    val shirst = listOf(
-            Product("Fallen Red Shirt","$55","redshirt"),
-            Product("Fallen Yellow Shirt","$60","yellowshirst"),
-            Product("Fallen Black Shirt","$50","blackshirt"),
-            Product("Fallen Grey Shirt","$55","greyshirt")
+    val shirts = listOf(
+            Product("Fallen Red Shirt", "$55", "redshirt"),
+            Product("Fallen Yellow Shirt", "$60", "yellowshirst"),
+            Product("Fallen Black Shirt", "$50", "blackshirt"),
+            Product("Fallen Grey Shirt", "$55", "greyshirt")
     )
+
+    val digitalProducts = listOf<Product>()
+
+    fun getProducts(category: String): List<Product> {
+        return when (category) {
+            "Shirts" -> shirts
+            "Hoodies" -> hoodies
+            "hats" -> hats
+            else -> digitalProducts
+        }
+    }
 }
